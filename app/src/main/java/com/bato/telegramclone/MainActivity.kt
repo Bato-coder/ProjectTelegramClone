579c8aaa180c2b0b628447538e53fe9a4dd68142
Bato Batoev <2702bato@gmail.com>
2021-01-22 07:03:31 +0900

    Add the libraries on Gradle

--- app/src/main/java/com/bato/telegramclone/MainActivity.kt
+++ app/src/main/java/com/bato/telegramclone/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bato.telegramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bato.telegramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
